fd9e9ca7fc71f712fa1d3b834ae018340066c8b5
Simon Bélanger-de Villers <simb.belanger@gmail.com>
2019-05-29 13:58:19 -0400

    revamping dev section

--- dev/Lab_Python/Feedback control/setup/methods/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This package contains methods used by the software.
-"""
-
-# modules imported with the " from Instruments import * " statement
-__all__ = []
-
-# directly import classes from modules
