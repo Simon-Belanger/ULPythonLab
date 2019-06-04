e784617fd5bed43c66f5554c8f6f01d54ea7c793
Simon Bélanger-de Villers <simb.belanger@gmail.com>
2019-06-04 14:38:12 -0400

    Added a GUI to the fiberarray util

--- dev/MRF_FBL/Algo.py
+++ dev/MRF_FBL/Algo.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+import pickle
 
 # Coordinates descent Algorithm 
 def sweep_bias(MRF,channel):
@@ -29,63 +30,104 @@ def sweep_bias(MRF,channel):
     MRF.PD.laser_off()
 
 # Coordinates descent Algorithm 
-def CoordsDescent(MRF,number_iter):
-    """Coarse tuning of a MRF object using the coordinates descent algorithm."""
+def CoordsDescent(MRF, number_iter, delay=0., mode='manual', plotPowMat=True):
+    """
+    Coarse tuning of a MRF object using the coordinates descent algorithm.
     
+    Args:
+        MRF (mrf): Microring filter object.
+        number_iter (int): number of iterations to perform.
+        delay (float): time delay between applying the bias and measuring the optical power [default = 0.1] .
+    
+    Returns:
+    
+    """
+    
+    x_i = [] # Bias applied
+    f_i = [] # Max power measured
+    power_mat = [[] for ii in range(MRF.num_parameters)] # Power map for each tuner
+
     # Turn on the laser and set the wavelength
-    MRF.PD.laser_on()
+    MRF.LMS.setTLSState('on')
+    time.sleep(5)
     for i in range(1,number_iter+1): # For each iteration
+        print("Progress update : Iteration #" + str(i) + " - Initiating ... ")
         for j in range(0,MRF.num_parameters): # For each ring
+            print("Progress update : Iteration #" + str(i) + " - Sweeping ring #" + str(j) + " ...")
             power_list = []
             
             # Possible bias values
-            bias_min = 0
-            bias_max = MRF.limitDC[j]
-
-            bias_points = 500
-            bias_testpoints = np.linspace(bias_min,bias_max,bias_points).tolist()
+            bias_testpoints = np.arange(MRF.LowerLimitDC[j],MRF.UpperLimitDC[j]+MRF.ResolutionDC[j],MRF.ResolutionDC[j]).tolist()
             
             for k in bias_testpoints: # For each bias value
                 MRF.apply_bias(j+1,k)
-                time.sleep(0.2)
-                power_list.append(MRF.PD.measure_power())            
-            plotsweep(bias_testpoints, power_list)
+                time.sleep(delay)
+                power_list.append(MRF.LMS.readPWM(2, MRF.PWMchannel))            
+            power_mat[j].append(power_list)
             
-            # pour choisir une valeur autre que le minimum
-            bias_voulu = raw_input('Enter your bias:')
-            if bias_voulu == "min":
-                MRF.apply_bias(j+1,bias_testpoints[power_list.index(min(power_list))])
-            else:
-                MRF.apply_bias(j+1,bias_voulu)
-                
+            # Depending on the chosen mode
+            if mode == 'manual': # Shows the power vs bias plot
+                plotsweep(bias_testpoints, power_list)
+                # pour choisir une valeur autre que le minimum
+                bias_voulu = raw_input('Enter your bias:')
+                if bias_voulu == "min": # Easy command to get the minimal value
+                    chosen_bias = bias_testpoints[power_list.index(min(power_list))]
+                elif bias_voulu == "max": # Easy command to get the maximal value
+                    chosen_bias = bias_testpoints[power_list.index(max(power_list))]
+                else: # Custom value
+                    chosen_bias = bias_voulu
+            elif mode == 'min': # Always choose the minimum value
+                chosen_bias = bias_testpoints[power_list.index(min(power_list))]
+            elif mode == 'max': # Always choose the maximum value
+                chosen_bias = bias_testpoints[power_list.index(max(power_list))]
+            MRF.apply_bias(j+1, chosen_bias)    
+        
+        # Save the iteration Log
+        x_i.append(MRF.applied_bias)
+        f_i.append(max(power_list))
+
     # Turn on the laser 
-    MRF.PD.laser_off()
+    MRF.LMS.setTLSState('off')
+    
+    # Plot power map
+    if plotPowMat == True:
+        plotPowMap(MRF, power_mat)
+        pickle.dump( power_mat, open( MRF.data_dir + "power_mat.p", "wb" ) )
+    
+    return x_i, f_i
             
 # Nelder Mead simplex algorithm
-def NelderMead(MRF,x0=[]):
+def NelderMead(MRF, x0=[], port='max'):
     """Fine tuning of a MRF object using the Nelder Mead simplex algorithm"""
+    
     # Turn on the source
-    MRF.PD.laser_on()
+    MRF.LMS.setTLSState('on')
     # Initial guess
     if x0 == []:
         x0 = MRF.applied_bias
+    
     # Optimization
-    res = minimize(MRF.maximize_MRF, x0, method='Nelder-Mead', options={'disp': True, 'xatol': 0.0001, 'maxiter': 100, 'maxfev': 100})
+    if port == 'max':
+        res = minimize(MRF.Drop_function, x0, method='Nelder-Mead', options={'disp': True, 'xtol': 0.0001, 'maxiter': 100, 'maxfev': 100})
+    elif port == 'min':
+        res = minimize(MRF.Thru_function, x0, method='Nelder-Mead', options={'disp': True, 'xtol': 0.0001, 'maxiter': 100, 'maxfev': 100})
+     
     #Turn off the source
-    MRF.PD.laser_off()
+    MRF.LMS.setTLSState('off')
     return res.x
 
-def tuneMRF(MRF, wavelength):
+def tuneMRF(MRF, wavelength, delay=0.1, port='max'):
     """Tune/stabilise the MRF object using coarse + fine algorithms"""
    
-    MRF.PD.set_wavelength(wavelength)
+    MRF.LMS.setTLSWavelength(wavelength*1e-9)
     
-    CoordsDescent(MRF,2) # Coordinates descent
-    NelderMead(MRF,[]) # Nelder Mead
+    CoordsDescent(MRF, 2, delay, port) # Coordinates descent
+    NelderMead(MRF,[], port) # Nelder Mead
     
 def plotsweep(bias, power):
     plt.plot(bias, power)
     plt.plot([bias[power.index(min(power))]], [min(power)], marker='o', markersize=10, color="red")
+    plt.plot([bias[power.index(max(power))]], [max(power)], marker='o', markersize=10, color="red")
     plt.xlabel("Bias [V]")
     plt.ylabel("Power [dBm]")
     plt.show()
@@ -93,6 +135,25 @@ def plotsweep(bias, power):
 def plotsweep2(ax, bias, power):
     ax.plot(bias, power)
     ax.plot([bias[power.index(min(power))]], [min(power)], marker='o', markersize=10, color="red")
+    ax.plot([bias[power.index(max(power))]], [max(power)], marker='o', markersize=10, color="red")
     ax.xlabel("Bias [V]")
     ax.ylabel("Power [dBm]")
     return ax
+
+def plotconvergence(f_i):
+    plt.plot(range(1,len(f_i)+1), f_i, marker='o', markersize=10, color="black")
+    plt.xlabel("Number of iterations")
+    plt.ylabel("Power [dBm]")
+    plt.show()
+    
+def plotPowMap(MRF, power_mat):
+    for i in range(0,len(power_mat)): # For each tuner
+        bias_testpoints = np.arange(MRF.LowerLimitDC[i],MRF.UpperLimitDC[i]+MRF.ResolutionDC[i],MRF.ResolutionDC[i]).tolist()
+        for j in range(0,len(power_mat[i])): # For each iteration
+            legend_label = "Iteration " + str(j+1)
+            plt.plot(bias_testpoints,power_mat[i][j],label=legend_label)
+            plt.xlabel("Bias [V]")
+            plt.ylabel("Power [dBm]")
+            plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        plt.show()
+    
---
+++ dev/fiberarray.py
@@ -0,0 +1,159 @@
+#TODO : Interaction with the lid and limitations due to it.
+#TODO : Manage collisions between the fiber array and the chip (max angle)
+
+
+import math
+from matplotlib import pyplot as plt
+from shapely.geometry.polygon import LinearRing, Polygon, LineString
+from shapely import affinity
+from matplotlib.widgets import Slider
+
+class fiber_array(object):
+    """ Class that implements a fiber array in order to calculate the array angle required for the right incidence angle
+    to the grating coupler. 
+    
+    Simon Bélanger-de Villers
+    August 2018
+
+    Added graphical UI in June 2019
+    """
+
+    n_glass = 1.4       # Index of refraction of silica glass [-]
+    n_air   = 1.        # Index of refraction of air [-]
+
+    array_width     = 30    # Total thickness of the fiber array [µm]
+    array_heigth    = 100   # Total height of the fiber array [µm]
+    lid_thickness   = 10    # Thickness of the lid [µm]
+    vertical_offset = 100    # Vertical offset between the corner of the top of the lid and the chip [µm]
+    
+    def __init__(self, name = "name", pol = 'TE', num_IO = 12, theta_offset = 330., theta_array = 10., theta_polish = 30., pitch = 127):
+        self.name           = name
+        self.pol            = pol
+        self.num_IO         = num_IO
+        self.pitch          = pitch
+
+        self.theta_offset   = theta_offset
+        self.theta_polish   = theta_polish # Between 0 and 45 degrees
+        self.theta_array    = theta_array
+        
+        self.theta_refrac = self.theta_refrac()
+        self.theta_inc_max = self.theta_incidence_max()
+
+        self.theta_incidence = self.theta_array + self.theta_refrac
+        
+    def theta_refrac(self):
+        """ Compute the refraction angle out of the fiber array the given polish angle and materials. """
+        theta_refrac = math.degrees(math.asin((self.n_glass/self.n_air) * math.sin(math.radians(self.theta_polish)))) - self.theta_polish
+        return theta_refrac
+    
+    def measure_theta_array(self, theta_incidence):
+        """ For a given incidence angle, compute the required array angle. """
+        theta_array = theta_incidence - self.theta_refrac
+        return theta_array
+    
+    def theta_incidence_max(self):
+        """ Find the maximum allowable incidence angle for a given fiber polish. 
+            Could find this geometrically using shapely also.
+        """
+        theta_inc_max = self.theta_polish + self.theta_refrac
+        return theta_inc_max
+    
+    def measure_theta_measured(self, theta_incidence):
+        """ For a given incidence angle, compute the required measured angle on the setup. """
+        theta_measured = theta_incidence - self.theta_refrac + self.theta_offset
+        return round(theta_measured,2)
+    
+    def measure_theta_incidence(self, theta_measured):
+        """ For a given measured angle on the setup, compute the incidence angle on the chip. """
+        theta_incidence = theta_measured - self.theta_offset + self.theta_refrac
+        return theta_incidence
+
+    def draw_shapes(self, axes):
+        """ Draw the fiber array and the light ray from the fiber array to the chip. """
+        
+        # Build the fiber array from a rectangle with a polish angle (bevel)
+        rect = Polygon([(0, 0), (self.array_width, 0), (self.array_width, self.array_heigth), (0, self.array_heigth)])
+        bevel = Polygon([(0, 0), (self.array_width, 0), (0, self.array_width*math.tan(math.radians(self.theta_polish)))])
+        if self.theta_polish != 0:
+            far = rect.difference(bevel)
+        else:
+            far = rect
+        far = affinity.rotate(far, self.theta_array, origin=(0, self.array_heigth)) # Rotate the array angle
+        far = affinity.translate(far, yoff=self.vertical_offset) # Vertically offset the fiber array
+        axes.plot(far.exterior.xy[0], far.exterior.xy[1], color="b")
+        
+        # Ray from the fiber
+        ray = LineString([(self.array_width-self.lid_thickness, self.array_heigth), (self.array_width-self.lid_thickness, self.lid_thickness*math.tan(math.radians(self.theta_polish)))])
+        ray = affinity.rotate(ray, self.theta_array,(0, self.array_heigth))
+        ray = affinity.translate(ray, yoff=self.vertical_offset)
+        axes.plot(ray.xy[0], ray.xy[1], color="r")
+        
+        # Ray free space
+        ray2 = LineString([ray.coords[1], (ray.coords[1][0]+ray.coords[1][1]*math.tan(math.radians(self.theta_incidence)), 0)])
+        axes.plot(ray2.xy[0], ray2.xy[1], color="r")
+        
+        # The chip
+        chip = Polygon([(-100, 0), (100, 0), (100, -20), (-100, -20)])
+        axes.plot(chip.exterior.xy[0], chip.exterior.xy[1], color="k")
+        
+        # Horizontal line
+        hline = LineString([(0, self.array_heigth+self.vertical_offset), (0,-20)])
+        axes.plot(hline.xy[0], hline.xy[1], color="k", linestyle="dashed")
+
+    def show_util(self):
+
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(left=0.25, bottom=0.35)
+
+        ax.axis([-200, 200, -50, 200])
+
+        ax_arrayangle = fig.add_axes([0.25, 0.25, 0.65, 0.03]) # [left, bottom, width, height] 
+        ax_polishangle = fig.add_axes([0.25, 0.15, 0.65, 0.03]) 
+        ax_verticaloff = fig.add_axes([0.25, 0.05, 0.65, 0.03]) 
+        s_arrayangle = Slider(ax_arrayangle, 'Array\n angle [°]', 0, 30, valinit=self.theta_array)
+        s_polishangle = Slider(ax_polishangle, 'Polish\n angle [°]', 0, 45, valinit=self.theta_polish)
+        s_verticaloff = Slider(ax_verticaloff, 'Vertical\n offset [µm]', -100, 100, valinit=self.vertical_offset)
+
+        self.draw_shapes(ax)
+
+        def update(val):
+            self.theta_array = s_arrayangle.val 
+            self.theta_polish = s_polishangle.val 
+            self.vertical_offset = s_verticaloff.val 
+            self.draw_shapes(ax)
+            del ax.lines[0:-6]
+            fig.canvas.draw()
+
+        s_arrayangle.on_changed(update)
+        s_polishangle.on_changed(update)
+        s_verticaloff.on_changed(update)
+
+        plt.show()
+
+
+
+    def __repr__(self):
+        log = """Fiber Array object with properties:
+        name : %s
+        polarization : %s
+        number of I/O : %d
+        pitch : %d um
+        polish angle : %2.2f deg
+        array offset angle : %3.2f deg
+        maximum incidence angle : %2.2f deg
+        """ % (self.name, self.pol, self.num_IO, self.pitch, self.theta_polish, self.theta_offset, self.theta_inc_max)
+        return log
+
+if __name__ == '__main__':
+
+    far = fiber_array(name = "Aeponyx_24", theta_array = 10., theta_polish = 20.)
+    print(far)
+
+    #print("Incidence angle is 20 deg, measured angle must be: " + str(far.theta_measured(20)) + " deg.")
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)
+    #far.draw_shapes(ax)
+    #ax.axis('scaled')
+    #plt.show()
+
+    far.show_util()
